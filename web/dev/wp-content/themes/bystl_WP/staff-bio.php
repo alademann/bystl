0b1afbee1a5b70039b0126c9263ccb339836d98a
Aaron Lademann <aaron.lademann@webfilings.com>
2012-08-18 14:07:59 -0700

    new staff bio single page template

---
+++ web/dev/wp-content/themes/bystl_WP/staff-bio.php
@@ -0,0 +1,33 @@
+<?php /*
+  Template Name: Staff Bio
+ */ ?>
+
+<?php get_header();
+// ends indented one... indent all children two
+?>
+
+        <div class="page-container row-fluid clearfix">
+
+            <div class="page-content full span16">
+                <?php
+                if (have_posts ()) {
+                    while (have_posts ()) {
+                        the_post();
+                        echo do_shortcode("[connections template='profile-individual' slug='".$post->post_name."']");
+                    } // END while()
+                } else { ?>
+
+                <div class="post box">
+                    <h3><?php _e('There is not bio for this staff member.', 'localization'); ?></h3>
+                </div>
+
+                <?php
+                } // END if()
+                ?>
+
+            </div>
+            <!-- END: .page-content.full -->
+        </div>
+        <!-- END: .page-container -->
+
+<?php get_footer(); ?>
