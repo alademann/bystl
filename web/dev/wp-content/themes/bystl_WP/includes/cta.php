027850732816b672e08eeedc00d14ff36bf3c4d5
Aaron Lademann <aaron.lademann@webfilings.com>
2012-08-26 20:12:32 -0700

    replacing synapse style button with bootstrap in cta

--- web/dev/wp-content/themes/bystl_WP/includes/cta.php
+++ web/dev/wp-content/themes/bystl_WP/includes/cta.php
@@ -1,6 +1,6 @@
 <div class="container-fluid">
-  
-  
+
+
     <div class="callout row-fluid clearfix">
 
       <div class="callout-info">
@@ -10,13 +10,13 @@
       </div>
 
     <?php if (get_option_tree('cta_btn') == 'Yes') { ?>
-      
 
-        <a class="big-btn blue rounded-1" href="<?php echo get_option_tree('cta_btn_url') ?>"><?php echo get_option_tree('cta_btn_text') ?></a>
+
+        <a class="btn btn-large btn-primary" href="<?php echo get_option_tree('cta_btn_url') ?>" style="float: right; margin-top: 12px;"><?php echo get_option_tree('cta_btn_text') ?></a>
 
 
     <?php } ?>
     </div>
 
-  
+
 </div>
