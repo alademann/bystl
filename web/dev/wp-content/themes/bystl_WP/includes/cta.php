a22ed294c9a9bd3e7047c3602141e1f94d2f0d28
Aaron Lademann <aaron.lademann@webfilings.com>
2012-09-30 09:47:45 -0700

    changed call-to-action button on homepage to open mbol modal functionality

--- web/dev/wp-content/themes/bystl_WP/includes/cta.php
+++ web/dev/wp-content/themes/bystl_WP/includes/cta.php
@@ -12,7 +12,7 @@
     <?php if (get_option_tree('cta_btn') == 'Yes') { ?>
 
 
-        <a class="btn btn-large btn-primary" href="<?php echo get_option_tree('cta_btn_url') ?>" style="float: right; margin-top: 12px;"><?php echo get_option_tree('cta_btn_text') ?></a>
+        <a class="btn btn-large btn-primary" data-toggle="modal" data-target="#mbolModal" href="<?php echo get_option_tree('cta_btn_url') ?>" style="float: right; margin-top: 12px;"><?php echo get_option_tree('cta_btn_text') ?></a>
 
 
     <?php } ?>
